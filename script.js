eeed982a33140304bc7128b774e452329bfc2480
Torazo5 <torazo.tokuda@gmail.com>
2023-11-13 23:10:28 +0800

    animation for intro section

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Add an event listener to the window object that will call the scrollFunction whenever the user scrolls
+  window.addEventListener("scroll", scrollFunction);
+
+  function scrollFunction() {
+    // Get the current scroll position
+    var scrollPosition = window.scrollY;
+
+    // Get the intro section element
+    var introSection = document.getElementById("intro");
+
+    // Define the scroll position at which the animation should start (adjust as needed)
+    var animationStart = 10;
+
+    // Check if the scroll position is greater than the animation start position
+    if (scrollPosition > animationStart) {
+      // If it is, add the "fade-slide-in" class to trigger the animation
+      introSection.classList.add("fade-slide-in");
+    }
+  }
+});
